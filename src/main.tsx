7678dc2ab697fec39b113319f5f0b46fc5b47eeb
Weronika Olejniczak <wer.olejniczak@gmail.com>
2023-11-19 16:15:54 +0100

    chore: use Sass preprocessor
    - use Sass preprocessor
    - divide index stylesheet into root and main

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './app.tsx';
-import './index.css';
+import './styles/root.scss';
+import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
